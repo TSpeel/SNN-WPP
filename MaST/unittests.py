ca962596ff5a557f1b32f26c6d9aa989d18cc6c3
Ties <ties.speel@gmail.com>
2024-11-27 13:39:56 +0100

    Extra unit test

--- MaST/unittests.py
+++ MaST/unittests.py
@@ -69,6 +69,7 @@ MaST:
         self.assertEqual(result.stdout.strip(), expected_output.strip())
 
 class MaSTTest3(unittest.TestCase):
+#Case where input is already a MiST and MaST. Thus this will pass for both a MiST and MaST
     def test_output_with_input_array(self):
         cmd = [
             sys.executable,
@@ -97,6 +98,37 @@ MaST:
  [0. 0. 0. 3. 0.]
  [0. 0. 0. 0. 0.]
  [0. 0. 0. 0. 0.]]
+'''
+        
+        self.assertEqual(result.stdout.strip(), expected_output.strip())
+
+
+class MaSTTest4(unittest.TestCase):
+    def test_output_with_input_array(self):
+        cmd = [
+            sys.executable,
+            os.path.join(os.path.dirname(__file__), 'constructNetwork.py'),
+            '-i',
+            '[[0,2,5],[0,0,1],[0,0,0]]'
+        ]
+        result = subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        
+        expected_output = '''
+Graph:
+[[0 2 5]
+ [0 0 1]
+ [0 0 0]]
+
+
+MaST:
+[[0. 2. 5.]
+ [0. 0. 0.]
+ [0. 0. 0.]]
 '''
         
         self.assertEqual(result.stdout.strip(), expected_output.strip())
