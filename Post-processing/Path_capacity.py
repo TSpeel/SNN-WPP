24e54f9cde2504c6d68ea06df526b6ec1d68f4ac
Pipapaula <77072601+Pipapaula@users.noreply.github.com>
2024-12-16 13:04:18 +0100

    Added weight mapping in construct function
    If input matrix has duplicate weights, perform mapping to create unique weight matrix. Otherwise assign original input matrix to the unique weight matrix. 
    Errors occur when there are duplicate input weights but only for particular instances...

--- Post-processing/Path_capacity.py
+++ Post-processing/Path_capacity.py
@@ -19,11 +19,13 @@ class Constructor():
         self.nr_nodes, _ = weight_matrix.shape
         self.nr_edges = np.count_nonzero(weight_matrix)
         self.neurons = []
+        self.synapses_track = []
+        self.synapses_net = []
         self.tree = {}
         self.root = root
         self.final = final
         self.target_nodes = []
-        #self.max_weight = np.max(weight_matrix)
+        self.max_weight = np.max(weight_matrix)
         self.total_weight = np.sum(weight_matrix)
 
 
@@ -63,17 +65,20 @@ class Constructor():
                     delay = max(adj_matrix[node][neighbor], adj_matrix[neighbor][node]) # delay is "weight" of connection
                     # create synapses; from one node (neighbor) to its previous node (node) and to the tracking neuron:
                     self.network.createSynapse(self.neurons[neighbor], self.neurons[node], w=1, d=delay)
+                    self.synapses_net.append(self.network.createSynapse(self.neurons[neighbor], self.neurons[node], w=1, d=delay))
                     self.network.createSynapse(self.neurons[neighbor], tracking_neuron, w=1, d=delay)
+                    self.synapses_track.append(self.network.createSynapse(self.neurons[neighbor], tracking_neuron, w=1, d=delay))
 
-                    #print("neighbor =", neighbor, "node =", node, "delay =", delay)
+
+                    print("neighbor =", neighbor, "node =", node, "delay =", delay)
                     
                     visited[neighbor] = True
                     queue.append(neighbor)
 
+        print("nr. synapses to track =", len(self.synapses_track), "nr. synapses in net =", len(self.synapses_net))
+
         return dict(tree)
-
-
-
+    
     def construct(self):
         self.network = Network()
         counter_neurons = 3 # to id all neurons, 1 and 2 are for tracking- and start neurons
@@ -81,13 +86,55 @@ class Constructor():
         for i in range(self.nr_nodes):
             neuron = self.network.createLIF(m=0, V_init=0, V_reset=0, thr=1, read_out=False, ID=counter_neurons)
             self.neurons.append(neuron)
+            counter_neurons += 1
+            
+        # If weights are not unique, make them so
+        # first check if weights are unique:
+        import collections
+        weights = [self.weight_matrix[i][j] for i in range(self.nr_nodes) for j in range(self.nr_nodes) if self.weight_matrix[i][j] != 0]
+        unique_weights = list(collections.Counter(weights))
 
-        # Calling build_tree builds the tree and creates the synapses between the neurons just created
-        self.tree = self.build_tree(weight_matrix)
+        if len(unique_weights) < len(weights):
+            # no unique weights, must perform the mapping:
+            counter = 1
+            self.weight_matrix_transformed = np.zeros((self.nr_nodes, self.nr_nodes))
+            #weight_matrix_transformed = [[0 for i in range(self.nr_nodes)] for j in range(self.nr_nodes)]
+            unique_weights, weight_counts = np.unique(self.weight_matrix, return_counts=True)
+            max_duplicate_weights = weight_counts[1] if 0 in unique_weights else weight_counts[0]
+
+            weight_history = []
+            for i in range(self.nr_nodes):
+                for j in range(i+1, self.nr_nodes):
+                    w = self.weight_matrix[i,j]
+                    if i != j and w > 0:
+                        weight_count = np.count_nonzero(weight_history == w)
+                        weight = int(float(max_duplicate_weights * w + weight_count))
+                        weight_history.append(w)
+                        self.weight_matrix_transformed[i,j] = weight
+                        self.weight_matrix_transformed[i,j] = int(float(self.weight_matrix_transformed[i,j]))
+
+                        # Increase counter
+                        counter+=1
+
+                        # Keep track of maximum delay
+                        if weight > self.max_weight:
+                            self.max_weight = weight
+            
+            self.weight_matrix_transformed = self.weight_matrix_transformed.astype(int) # to convert float.64 entries to integers
+        
+        else:
+            # if weights are already unique: assign the orignal matrix to the unique matrix
+            self.weight_matrix_transformed = self.weight_matrix
+        
+        print("Unique weight matrix =", self.weight_matrix_transformed)
+
+         # Calling build_tree builds the tree and creates the synapses between the neurons just created
+        self.tree = self.build_tree(self.weight_matrix_transformed)
         print("Tree =", self.tree)
         # the input train starts the interation and has to be connected to the desired final neuron:
         start_train = self.network.createInputTrain([1], loop=False, ID=2)
         self.network.createSynapse(start_train, self.neurons[self.final], w=1, d=1)
+        #print("self.final =", self.final)
 
         return self.network
 
@@ -97,10 +144,9 @@ def construct_widestpath(spikes, root, weight_matrix):
     cycle_starts = np.argwhere(spikes[:, 0] == True).flatten()
     # all subsequent delays in the temporal output pattern are:
     delay_list = [cycle_starts[0]-1] + [cycle_starts[i]-cycle_starts[i-1] for i in range(1,len(cycle_starts))]
-    print("Delays from temporal output tracking neuron:", delay_list)
-
+    print("Sequence of delays =", delay_list)
     capacity = np.min(delay_list) # capacity is the minimum "weight" which was translated into delays; the bottleneck in the path
-    #total_path_time = np.sum(delay_list) # the number of time-steps
+
 
     # construct tree manually again because we cannot call self in this function...
     # it is copy paste from the previous tree function, but without creating synapses
@@ -131,18 +177,22 @@ def construct_widestpath(spikes, root, weight_matrix):
                 queue.append(neighbor)
 
     # the tree is created, now use this tree to construct the path: which neurons are visited
-    count = 0
-    path = [root] # path starts at the root
+    nr_steps = len(delay_list) # In order to ensure the path is the correct length
+    path = [root]
+    print("root initialized =", root)
 
     for delay in delay_list[::-1]: # all delays encountered from root to final
         root = path[-1] # initializing the root as the last entry of the path
         children = tree[root] # this root bifurcates into its children, given by the tree
-
+        print("Delay =", delay, "root=", root, "children =", children)
         for child in children: # iterating over these chilren and only the child with corresponding delay is chosen
+            print("child =", child, "with delay =", weight_matrix[root,child], weight_matrix[child,root])
             if max(weight_matrix[root,child], weight_matrix[child,root]) == delay:
                  path.append(child) # this child is added to the path, in the next iteration this is chosen as new root
+        
 
-    spikes_at = [np.flatnonzero(spikes[:, node])[0] if spikes[:, node].any() else 99999 for node in range(3, spikes.shape[1])]
+    if len(path) != nr_steps + 1:
+        print("Path is incorrect")
 
     return capacity, path
 
@@ -150,7 +200,8 @@ def construct_widestpath(spikes, root, weight_matrix):
 def execute_simulator(weight_matrix, root, final):
     constructor = Constructor(weight_matrix, root, final)
     network = constructor.construct()
-    weights = weight_matrix
+    unique_weights = constructor.weight_matrix_transformed
+
     sim = Simulator(network)
     
     # Add all read_out neurons to the simulation
@@ -159,8 +210,9 @@ def execute_simulator(weight_matrix, root, final):
     sim.run(steps=constructor.total_weight+10, plotting=False)
 
     spikes = sim.raster.get_measurements()
+    print("nr of spikes:", np.count_nonzero(spikes))
 
-    return construct_widestpath(spikes, root, weight_matrix)
+    return construct_widestpath(spikes, root, unique_weights)
 
 
 parser = argparse.ArgumentParser()
@@ -169,11 +221,11 @@ args = parser.parse_args()
 if args.i is not None:
     weight_matrix = np.array(ast.literal_eval(args.i))
 else:
-    weight_matrix = np.array([[ 0,  0,  5,  0,  9],
-                      [ 0,  0,  0,  10,  0],
-                      [ 0,  0,  0,  0,  0],
-                      [ 0,  0,  0,  0,  2],
-                      [ 0,  0,  0,  0,  0]])
+    weight_matrix = np.array([[ 0,  0,  5,  0,  8],
+                              [ 0,  0,  0,  8,  0],
+                              [ 0,  0,  0,  0,  0],
+                              [ 0,  0,  0,  0,  6],
+                              [ 0,  0,  0,  0,  0]])
     
 #Usage eg python3 createMaST.py -i [[0,1,5,0,9],[0,0,2,7,0],[0,0,0,3,1],[0,0,0,0,14],[0,0,0,0,0]]
 # or just python3 createMaST.py
